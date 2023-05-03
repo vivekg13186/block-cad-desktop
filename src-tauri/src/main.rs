1478ad9068b424d5cddcf8e0bae6924109fac0f0
vivek g <vivek13186@gmail.com>
2023-05-03 17:38:50 +0200

    jkjk

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,13 +5,9 @@ use std::env;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
-use std::os::unix::prelude::FileExt;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+ 
 
 #[tauri::command]
 fn read_file(filepath: &str) -> String {
